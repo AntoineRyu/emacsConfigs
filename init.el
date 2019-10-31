f440966d158d07b06d9fe3527e271deeddc44183
Antoine Rioux <antoine.rioux@mujin.co.jp>
2019-10-31 18:09:35 +0900

    Speed up emacs init time

--- init.el
+++ init.el
@@ -1,3 +1,25 @@
+;; Speed up initialization. Suggestions taken from Doom emacs configs.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
+      gc-cons-percentage 0.6)
+
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 16777216 ; 16mb
+          gc-cons-percentage 0.1)))
+
+(defvar custom--file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq file-name-handler-alist custom--file-name-handler-alist)))
+
 (unless load-file-name
   (cd (getenv "HOME")))
 
