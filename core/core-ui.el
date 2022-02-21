11fb0bff0ec9f47112490f92f29eabbac6da549c
Antoine Rioux <antoine.rioux@mujin.co.jp>
2022-02-21 09:22:44 +0900

    Remove tabbar

--- core/core-ui.el
+++ core/core-ui.el
@@ -166,44 +166,5 @@
   (which-key-separator " ")
   (which-key-prefix-prefix "+"))
 
-;; TABBAR
-(use-package tabbar
-  :ensure t)
-
-(set-face-attribute
- 'tabbar-default nil
- :background "gray60")
-(set-face-attribute
- 'tabbar-unselected nil
- :background "gray85"
- :foreground "gray30"
- :box nil)
-(set-face-attribute
- 'tabbar-selected nil
- :background "#f2f2f6"
- :foreground "#FF1493"
- :box nil)
-(set-face-attribute
- 'tabbar-button nil
- :box '(:line-width 1 :color "gray72" :style released-button))
-(set-face-attribute
- 'tabbar-separator nil
- :height 0.7)
-
- (defun my-tabbar-buffer-groups () ;; customize to show all normal files in one group
-   "Returns the name of the tab group names the current buffer belongs to.
- There are two groups: Emacs buffers (those whose name starts with '*', plus
- dired buffers), and the rest.  This works at least with Emacs v24.2 using
- tabbar.el v1.7."
-   (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
-               ((eq major-mode 'dired-mode) "emacs")
-               (t "user"))))
- (setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
-
-(global-set-key (kbd "C-<tab>") 'tabbar-forward-tab)
-(global-set-key (kbd "C-S-<iso-lefttab>") 'tabbar-backward-tab)
-
-(tabbar-mode 1)
-
 (provide 'core-ui)
 ;;; core-ui.el ends here
